bead129bcca0bcd4cee732d8c9e67d904973646c
Shivaprasad84 <shivaprasadsmys@gmail.com>
2019-05-08 08:40:29 +0530

    replace, reverse and deletion of whole list

--- SigleLinkedList.cpp
+++ SigleLinkedList.cpp
@@ -20,12 +20,14 @@ private:
         newNode->next = NULL;
         return newNode;
     }
+    bool deleted;
 
 public:
     SingleLinkedList()
     {
         head = NULL;
         size = 0;
+        deleted = false;
     }
 
     void push_front(int x)
@@ -84,6 +86,22 @@ public:
         trav->next = newNode;
     }
 
+    void replace_at(int pos, int x)
+    {
+        if(pos == 0)
+        {
+            head->data = x;
+            return;
+        }
+        node* trav = head;
+        for(int i = 0; i < pos-1; i++)
+        {
+            trav = trav->next;
+        }
+        (trav->next)->data = x;
+    }
+
+
     int operator[](int index)
     {
         return this->at(index);
@@ -92,6 +110,13 @@ public:
     void delete_at(int pos)
     {
         size--;
+        if(pos == 0)
+        {
+            node* temp = head;
+            head = temp->next;
+            delete temp;
+            return;
+        }
         node* trav = head;
         for(int i = 0; i < pos - 1; i++)
         {
@@ -104,6 +129,11 @@ public:
 
     void display() const
     {
+        if(size == 0)
+        {
+            cout << "No Elements in List" << endl;
+            return;
+        }
         node *trav = head;
         for (int i = 0; i < size; i++)
         {
@@ -117,6 +147,41 @@ public:
     {
         return this->size;
     }
+
+    void reverse()
+    {
+       for(int i = size-1; i >= size/2; i--)
+       {
+           int temp = this->at((size-1) - i); // get first element
+           this->replace_at((size-1) - i, this->at(i)); // replace first element with last
+           this->replace_at(i, temp); // replace last element with first
+       }
+    }
+
+    void del_list()
+    {
+        node* trav = head;
+        node* temp;
+        while(trav->next != NULL)
+        {
+            temp = trav;
+            trav = trav->next;
+            delete temp;
+            size--;
+        }
+        delete trav;
+        size--; 
+        deleted = true;  
+    }
+
+    ~SingleLinkedList()
+    {
+        if(!deleted)
+        {
+            this->del_list();
+        }
+        cout << "List deleted: Releasing memory..." << endl;
+    }
 };
 
 int main()
@@ -129,6 +194,17 @@ int main()
 
     L1.insert_at(0, 100);
     L1.display();
-    L1.delete_at(3);
+    L1.reverse();
     L1.display();
+    L1.delete_at(0);
+    L1.display();
+    L1.replace_at(3, 999);
+    L1.display();
+    
+    cout << "Length: " << L1.getSize() << endl;
+
+    L1.del_list();
+
+    L1.display();
+
 }
